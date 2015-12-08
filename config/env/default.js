47004fc97a101e65e392ef41ef63b15145e4cd55
FisherYu <alexfisher051@gmail.com>
2015-12-08 16:38:18 +0800

    format code and add comment, also add part of less file for Inspinia theme

--- config/env/default.js
+++ config/env/default.js
@@ -1,32 +1,32 @@
 'use strict';
 
 module.exports = {
-  app: {
-    title: 'RiskControlPlatform',
-    description: 'TaiFinance Risk Control Platform',
-    keywords: 'MEAN.JS RiskControl',
-    googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
-  },
-  port: process.env.PORT || 3000,
-  templateEngine: 'swig',
-  // Session Cookie settings
-  sessionCookie: {
-    // session expiration is set by default to 24 hours
-    maxAge: 24 * (60 * 60 * 1000),
-    // httpOnly flag makes sure the cookie is only accessed
-    // through the HTTP protocol and not JS/browser 
-    httpOnly: true,
-    // secure cookie should be turned to true to provide additional
-    // layer of security so that the cookie is set only when working
-    // in HTTPS mode.
-    secure: false
-  },
-  // sessionSecret should be changed for security measures and concerns
-  sessionSecret: 'MEAN',
-  // sessionKey is set to the generic sessionId key used by PHP applications
-  // for obsecurity reasons
-  sessionKey: 'sessionId',
-  sessionCollection: 'sessions',
-  logo: 'modules/core/client/img/brand/logo.png',
-  favicon: 'modules/core/client/img/brand/favicon.ico'
+    // 应用基本信息
+    app: {
+        title: 'Risk Control Platform',
+        description: 'TaiFinance Risk Control Platform',
+        keywords: 'MEAN.JS RiskControl',
+        googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
+    },
+    // 运行端口
+    port: process.env.PORT || 3000,
+    // 模板引擎
+    templateEngine: 'swig',
+    // Session和Cookie设置
+    sessionCookie: {
+        // Session默认24小时过期
+        maxAge: 24 * (60 * 60 * 1000),
+        // 确保Session只可以通过HTTP协议访问，而不是通过JS或者浏览器
+        httpOnly: true,
+        // 启动安全Cookie将提供额外的安全保障，限制Cookie
+        // 只可以在HTTPS模式下被更改，默认关闭安全模式
+        secure: false
+    },
+    // Session秘钥
+    sessionSecret: 'TaiFinanceRCP',
+    // Session主键采用被PHP应用程序广泛使用的sessionId
+    sessionKey: 'sessionId',
+    sessionCollection: 'sessions',
+    logo: 'modules/core/client/img/brand/logo.png',
+    favicon: 'modules/core/client/img/brand/favicon.ico'
 };
